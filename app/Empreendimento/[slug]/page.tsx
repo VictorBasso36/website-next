f7828e4723072722380127ee0685e2ee65419d34
root <root@DESKTOP-SKBK36U>
2024-02-25 12:54:23 -0300

    fix

--- app/Empreendimento/[slug]/page.tsx
+++ app/Empreendimento/[slug]/page.tsx
@@ -32,6 +32,7 @@ export interface PropsGet {
   vagas?: string;
   imovel?: string;
   quartos?: number;
+  simpleName?: number;
   contrato?: string;
   img?: string;
   description?: string;
@@ -76,20 +77,25 @@ export default function Empreendimento({ params }: { params: { slug: string } })
               <div className={styles.cardInfo}>
                 <div className={styles.infosCardHere}>
                   <div className={styles.infoIconsHere}>
-
+                    {content?.size && <><p><Image src="/size.svg" width={20} height={15} alt="Tamanho dos imoveis da Vila Sul"></Image>{`${content?.size}`}</p></>}
+                    {content?.vagas && <><p><Image src="/vagas.svg" width={20} height={15} alt="Numero de vagas dos imoveis da Vila Sul"></Image>{`${content?.vagas}`}</p></>}
+                    {content?.imovel && <><p><Image src="/imovel.svg" width={20} height={15} alt="Tipo de imoveis da Vila Sul"></Image>{`${content?.imovel}`}</p></>}
+                    {content?.quartos && <><p><Image src="/rooms.svg" width={20} height={15} alt="quantidade de quartos imoveis da Vila Sul"></Image>{`${content?.quartos}`}</p></>}
+                    {content?.simpleName && <><p><Image src="/location.svg" width={20} height={15} alt="Localização dos imoveis da Vila Sul"></Image>{`${content?.simpleName}`}</p></>}
                   </div>
-                  <div className={styles.DetailHere}></div>
+                  <div className={styles.borderDetail}></div>
                   <div className={styles.TextHere}>
-                    <p><strong>{content?.endereco}</strong></p>
+                    <p className={styles.description}><strong>{content?.endereco}</strong></p>
                     <pre className={styles.textContent} dangerouslySetInnerHTML={{ __html: content?.description || '' }}>
                   
                     </pre>
                   </div>
+                  <br /><br />
                 </div>
                 <ButtonMain textstring="Conheça o Empreendimento"  />
               </div>
               {content?.fotos &&
-              <img src="/foto-de-um-predio-alto-e-alto_181624-2214.avif" alt={content?.fotos[0]?.description ? content?.fotos[0]?.description : ''} />}
+              <Image className={styles.mainImage} src="/foto-de-um-predio-alto-e-alto_181624-2214.avif" alt={content?.fotos[0]?.description ? content?.fotos[0]?.description : ''} width={500} height={700} />}
             </div>
           </div>
         </div>
